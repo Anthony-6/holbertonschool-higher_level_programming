b3d82a8ec92023118939b3ebb87e6cd11e07c000
Anthony-6 <anthony.lacoste5@gmail.com>
2021-12-23 21:42:45 +0100

    add file 2 args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print('0 arguments')
+else:
+    if len(argv) == 2:
+        print('1 argument:')
+    if len(argv) > 2:
+        print(len(argv)-1, 'arguments: ')
+    for arg in range(1, len(argv)):
+        print(arg, ':', argv[arg])
+if __name__ == "__main__":
+    len(argv)
