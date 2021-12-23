52934574ce7a140cce7af88e0c78908bf34b4ed7
Anthony-6 <anthony.lacoste5@gmail.com>
2021-12-23 23:33:39 +0100

    corrected import line

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys, hidden_4
+import sys
+import hidden_4
 for i in dir(hidden_4):
     if i[:2] != "__":
         print(i)
 if __name__ == "__main__":
-    sys, hidden_4
+    sys
+    hidden_4
