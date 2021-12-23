aa09571b169482596ff30b01b8f03d682843e678
Anthony-6 <anthony.lacoste5@gmail.com>
2021-12-23 22:04:39 +0100

    changed line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-if len(argv) <= 1:
-    print('0 arguments.')
-else:
-    if len(argv) == 2:
-        print(len(argv)-1, 'argument:')
-    if len(argv) > 2:
-        print(len(argv)-1, 'arguments: ')
-    for arg in range(1, len(argv)):
-        print(arg, ': ', argv[arg], sep="")
 if __name__ == "__main__":
-    len(argv)
+    from sys import argv
+    if len(argv) <= 1:
+        print('0 arguments.')
+    else:
+        if len(argv) == 2:
+            print(len(argv)-1, 'argument:')
+        if len(argv) > 2:
+            print(len(argv)-1, 'arguments: ')
+        for arg in range(1, len(argv)):
+            print(arg, ': ', argv[arg], sep="")
