557ad354dcc37959144d7edd94ac9ac2922d56b8
Anthony-6 <anthony.lacoste5@gmail.com>
2021-12-23 22:34:51 +0100

    hotfix file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if len(argv) == 1:
     print('0 arguments.')
 else:
     if len(argv) == 2:
-        print(len(argv)-1, 'argument:')
+        print('1 argument:')
     elif len(argv) != 2:
-        print(len(argv)-1, 'arguments: ')
+        print('arguments: {}'.format(len(argv)-1))
     for arg in range(1, len(argv)):
         print('{}: {}'.format(arg, argv[arg]))
 if __name__ == "__main__":
