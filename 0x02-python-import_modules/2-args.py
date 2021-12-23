eab4727e9ecaba234806641e5507acb98d87adab
Anthony-6 <anthony.lacoste5@gmail.com>
2021-12-23 21:55:49 +0100

    hotfix file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ else:
     if len(argv) > 2:
         print(len(argv)-1, 'arguments: ')
     for arg in range(1, len(argv)):
-        print(arg, ':', argv[arg])
+        print(arg,': ', argv[arg], sep="")
 if __name__ == "__main__":
     len(argv)
