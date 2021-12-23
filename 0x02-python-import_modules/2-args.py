c46719d9e0680009364391c5fb73711c89a2e5b6
Anthony-6 <anthony.lacoste5@gmail.com>
2021-12-23 22:00:18 +0100

    hotfix file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if len(argv) == 1:
     print('0 arguments.')
 else:
     if len(argv) == 2:
-        print('1 argument:')
+        print(len(argv)-1, 'argument:')
     if len(argv) > 2:
         print(len(argv)-1, 'arguments: ')
     for arg in range(1, len(argv)):
