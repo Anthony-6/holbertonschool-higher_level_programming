ae1bf37da0e079b310fd6b307893f565d57a4fb6
Anthony-6 <anthony.lacoste5@gmail.com>
2021-12-23 15:55:01 +0100

    corrected file 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-sum = a + b
-print('{0} + {1} = {2}'.format(a, b, sum))
-
 if __name__ == "__main__":
-    add(a, b)
+    from add_0 import add
+    a = 1
+    b = 2
+    sum = a + b
+    print('{0} + {1} = {2}'.format(a, b, sum))
