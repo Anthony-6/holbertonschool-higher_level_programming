ca99ce2f50011ce98541b0aad37bdd1748a8e41c
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-03 22:14:08 +0100

    hotfix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,10 +4,3 @@ def new_in_list(my_list, idx, element):
     temp[idx] = element
     if idx >= 0 and idx < len(temp):
         return temp
-    else:
-        return None
-    if idx < 0:
-        return temp
-    else:
-        return None
-        
