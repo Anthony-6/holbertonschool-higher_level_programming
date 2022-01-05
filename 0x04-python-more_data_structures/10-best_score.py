9aa96ba84cef78b56660b40a35188ee4fb64dda9
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-06 00:03:44 +0100

    hotfix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    else:
-        return max(a_dictionary)
+    max_dic = max(a_dictionary, key=lambda x: a_dictionary[x])
+    return max_dic
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
+    for i in a_dictionary:
+        if i == key:
+            del a_dictionary[i]
+            break
     return a_dictionary
