b337640339e84855c655fb9e060296d9d1c4fd79
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-17 15:56:12 +0100

    hotfix in the syntax; add .forma

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     try:
         value_int = isinstance(value, int)
         if value_int is True:
-            print(value)
+            print('{:d}'.format(value))
             return True
     except ValueError:
             return False
