0f58ff7ba44506e372f809981978845a5e3c599a
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-17 15:32:55 +0100

    hotfix file 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if ord(value) in range(48, 57):
-            return True
+        value_int = isinstance(value, int)
+        if value_int is True:
+            print(value)
+        return value_int
     except (TypeError, ValueError):
         return False
