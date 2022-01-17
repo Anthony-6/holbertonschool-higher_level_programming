1543b861e7584d47b52b7f3bf2445ee29db26e4d
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-17 14:49:22 +0100

    hotfix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,11 +6,9 @@ def safe_print_list(my_list=[], x=0):
         for elem in my_list[:x]:
             count += 1
         return count
-        safe_print_list(x)
     except TypeError:
         count = 0
         print("".join(map(str, my_list[:x])))
         for elem in my_list[:x]:
             count += 1
         return count
-        safe_print_list(x)
