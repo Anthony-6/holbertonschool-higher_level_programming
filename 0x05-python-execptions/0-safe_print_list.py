958f6e74b059e45a19b2cd7438e0a4fe7595aa2a
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-17 14:02:11 +0100

    add file

---
+++ 0x05-python-execptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    print("".join(map(str, my_list[:x])))
+    for elem in my_list[:x]:
+        count += 1
+    return count
+    safe_print_list(x)
