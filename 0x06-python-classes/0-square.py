415d2a49752a48b6b85c592cb9597b1c45e5d8be
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-19 21:45:54 +0100

    add file from 0 to 4

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''0-square.py
+
+Write an empty class 'Square' that defines a square
+'''
+
+
+class Square:
+    ''' this class defines a square'''
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Write a class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)'''
+
+
+class Square:
+    '''this class declare a square'''
+    def __init__(self, size):
+        self.__size = size
