0e6113612e013e9f02af9f5fd69cc1b76b68f22f
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-24 19:08:10 +0100

    add file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''0-rectangle.py
+Write an empty class 'Rectangle' that defines a retangle
+'''
+
+
+class Rectangle:
+    ''' this class defines a square'''
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+class Rectangle:
+    '''this class define a rectangle
+        with width and height to define the size of the rectangle
+    '''
+    valuemess = 'height must be >= 0'
+    typemess = 'height must be an integer'
+
+    def __init__(self, width=0, height=0):
+        self.__width = (width)
+        if type(width) is not int:
+            raise TypeError(typemess)
+        elif width < 0:
+            raise ValueError(valuemess)
+        self.__height = (height)
+        if type(height) is not int:
+            raise TypeError(typemess)
+        elif height < 0:
+            raise ValueError(valuemess)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError(typemess)
+        elif value < 0:
+            raise ValueError(valuemess)
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError(typemess)
+        elif value < 0:
+            raise ValueError(valuemess)
+        self.__width = value
