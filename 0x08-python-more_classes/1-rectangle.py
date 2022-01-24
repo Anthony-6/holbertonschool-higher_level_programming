df9055fd49064fd7897b5e6c764e806365dd9914
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-24 19:25:22 +0100

    fix file 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,16 +7,17 @@ class Rectangle:
     typemess = 'height must be an integer'
 
     def __init__(self, width=0, height=0):
-        self.__width = (width)
+
         if type(width) is not int:
             raise TypeError(typemess)
         elif width < 0:
             raise ValueError(valuemess)
-        self.__height = (height)
         if type(height) is not int:
             raise TypeError(typemess)
         elif height < 0:
             raise ValueError(valuemess)
+        self.__height = height
+        self.__width = width
 
     @property
     def width(self):
@@ -40,4 +41,4 @@ class Rectangle:
             raise TypeError(typemess)
         elif value < 0:
             raise ValueError(valuemess)
-        self.__width = value
+        self.__height = value
