8293a4306f39d13f0ca79f4d37f50fcdabd66a03
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-26 03:37:10 +0100

    hotfix

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+'''
+    -Prototype: def print_square(size):
+    -size is the size length of the square
+    -size must be an integer, otherwise raise
+    a TypeError exception with the message size must be an integer
+    -if size is less than 0, raise a ValueError exception with the
+    message size must be >= 0
+'''
+
+
 def print_square(size):
+    '''
+    This function print a square with parameter size
+    '''
     if type(size) is not int:
         raise TypeError('size must be an integer')
     elif size < 0:
