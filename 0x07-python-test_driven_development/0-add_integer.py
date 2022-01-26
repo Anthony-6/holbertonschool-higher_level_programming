c5ea3cf4532344ea40bde9ea1471a023622d586f
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-26 01:21:11 +0100

    hotfix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+    '''
+    This function add two integers
+
+    -Prototype: def add_integer(a, b=98):
+    -a and b must be integers or floats, otherwise raise
+     a TypeError exception with the message a must be an
+     integer or b must be an integer
+    -a and b must be first casted to integers if they are float
+    -Returns an integer: the addition of a and b
+    '''
 def add_integer(a, b=98):
-    '''
-    doctext a completer plus tard
-    '''
 
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
