0b81d079fd9ab9f54b703895c710d3bd8d8500b5
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-31 22:47:18 +0100

    hotfix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,6 +11,6 @@ def inherits_from(obj, a_class):
     '''
     if type(obj) != a_class:
         if isinstance(type(obj), a_class):
-            return False
-        else:
             return True
+        else:
+            return False
