6000125601dbbbaf2e959f319d60ed82449dfb3f
Anthony-6 <anthony.lacoste5@gmail.com>
2022-02-01 10:43:21 +0100

    add first file

---
+++ 0x0B-python_input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+function that reads a text file (UTF8) and prints it to stdout:
+
+Prototype: def read_file(filename=""):
+You must use the with statement
+You don’t need to manage file permission or file doesn't exist exceptions.
+'''
+
+
+def read_file(filename=""):
+    '''
+    this function open a file called my_file_0.txt and read it
+    '''
+    with open('my_file_0.txt', encoding='UTF-8') as myFile:
+        print(myFile.read())
