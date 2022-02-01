42fa7e6e0d9b9ff64a3034e4c3eb9abfd0dd4f44
Anthony-6 <anthony.lacoste5@gmail.com>
2022-02-01 13:16:59 +0100

    add file 5

---
+++ 0x0B-python_input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+function that writes an Object to a text file
+using a JSON representation
+
+-Prototype: def save_to_json_file(my_obj, filename):
+-You must use the with statement
+-You don’t need to manage exceptions if the
+ object can’t be serialized.
+'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''
+
+    '''
+    with open(filename, mode='w', encoding='UTF-8') as myFile:
+        return myFile.write(json.dumps(my_obj))
