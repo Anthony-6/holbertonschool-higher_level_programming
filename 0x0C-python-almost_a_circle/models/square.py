7887353c3f4fe07653c492d709411686360b0e8f
Anthony-6 <anthony.lacoste5@gmail.com>
2022-02-07 15:42:33 +0100

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,13 +11,17 @@ In the file models/square.py
 Class Square inherits from Rectangle
 Class constructor: def __init__(self, size, x=0, y=0, id=None)::
 Call the super class with id, x, y, width and height - this super call will
-use the logic of the __init__ of the Rectangle class. The width and height must be assigned
+use the logic of the __init__ of the Rectangle class. The width and height
+must be assigned
 to the value of size
-You must not create new attributes for this class, use all attributes of Rectangle -
+You must not create new attributes for this class, use all attributes of
+Rectangle -
 As reminder: a Square is a Rectangle with the same width and height
-All width, height, x and y validation must inherit from Rectangle - same behavior in case
+All width, height, x and y validation must inherit from Rectangle - same
+behavior in case
 of wrong data
-The overloading __str__ method should return [Square] (<id>) <x>/<y> - <size> - in our case,
+The overloading __str__ method should return [Square] (<id>) <x>/<y> - <size> -
+in our case,
 width or height
 '''
 
