07ae25c3d56e52a90639c6fb296297421aca49f4
Anthony-6 <anthony.lacoste5@gmail.com>
2022-02-07 14:38:18 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 from models.base import Base
+'''
+class Rectangle that inherits from Base:
+
+In the file models/rectangle.py
+Class Rectangle inherits from Base
+Private instance attributes, each with its own public getter and setter:
+__width -> width
+__height -> height
+__x -> x
+__y -> y
+Class constructor: def __init__(self, width, height, x=0, y=0, id=None):
+Call the super class with id - this super call with use the logic of the
+__init__ of
+the Base class
+Assign each argument width, height, x and y to the right attribute
+'''
 
 
 class Rectangle(Base):
