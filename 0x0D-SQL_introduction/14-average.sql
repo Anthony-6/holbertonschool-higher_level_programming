3a2acf204be863252bd056c5d589120d046d854c
anthony <Anthony-6>
2022-03-08 13:21:52 +0100

    update file 14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,6 @@
 -- This script gave us the average of score
 -- This gace us the average score of second table
 SELECT
-AVG(score) AS 'Average'
+AVG(score) AS 'average'
 FROM second_table
 WHERE score
