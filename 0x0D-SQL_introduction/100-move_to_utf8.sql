af1dd2ee76327dea960d15d7b44ce55c8c154ec8
anthony <Anthony-6>
2022-03-08 15:43:09 +0100

    git add file 100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- This script convert the entire database into utf8 format
+-- Convert databas into utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
