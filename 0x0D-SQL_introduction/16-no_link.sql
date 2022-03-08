9ebae4a737add6ba61807057be61e4f28e00a939
anthony <Anthony-6>
2022-03-08 13:54:53 +0100

    add file 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- This script display score and name in descending order
+-- Select score and name from the second_table table then sort it by descending order
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
