59871ed2ce361282aef109b04cc0ac953a15f781
anthony <Anthony-6>
2022-03-09 15:04:04 +0100

    fix code

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- This script create a database and a user with only select(read) permission if they didn't exists
 -- Create a database if not exists then the user, then the select access is granted to the user
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
