0bd4554398cd25f4de528cb3e7b0351013e8705a
Anthony-6 <anthony.lacoste5@gmail.com>
2022-03-09 16:37:12 +0100

    fix code on file 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- This script select tvshow titles and genre from the tv show tables and join the show id and the genres id then order them
 -- 
-FROM tv_shows SELECT tv_shows.titles, tv_shows_genres.genre_id
+FROM tv_shows SELECT tv_shows.title, tv_shows_genres.genre_id
 RIGHT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.titles, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
