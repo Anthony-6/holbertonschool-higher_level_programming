c34ac7cba8e0c759b274fccc82685cfe703fa60f
Anthony-6 <anthony.lacoste5@gmail.com>
2022-03-09 16:39:19 +0100

    add file

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- This script select tvshow titles and genre from the tv show tables and join the show id and the genres id then order them
+-- 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
