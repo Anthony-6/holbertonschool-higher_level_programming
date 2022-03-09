5d809f65f30e4daf8d82c17439d6da190410bc7a
anthony <Anthony-6>
2022-03-09 15:07:57 +0100

    fix error

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- This script create a new table based on the hbtn_0d_usa database if it didn't already exists
 -- Create the table state and set the default id to 1
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS state (
+CREATE TABLE IF NOT EXISTS states (
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY(id)
