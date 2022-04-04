1089777788d57ec2c47df083ba6f721f57b68b1c
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-04 18:39:18 +0200

    update file 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const arg = process.argv;
 const isInt = parseInt(arg[2]);
 if (isNaN(isInt)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else { 
   for (let i = 0; i < arg[2]; i++) {
     console.log('X'.repeat(arg[2]));
