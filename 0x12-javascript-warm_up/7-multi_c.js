2ad592321688abd8c41f10bb8ef8b6a8c7690373
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-04 18:05:46 +0200

    update 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const isInt = parseInt(arg[2]);
 if (isNaN(isInt)) {
   console.log('Missing number of occurrences');
 } else { 
-  for (i = 0; i < arg[2]; i++) {
+  for (let i = 0; i < arg[2]; i++) {
     console.log('C is fun');
   }
 }
