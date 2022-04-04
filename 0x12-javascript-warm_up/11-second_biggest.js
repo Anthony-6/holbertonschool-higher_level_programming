5d6a16acfd9e6ef5817ed3c8d1095dd515ff626d
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-04 22:42:49 +0200

    update file 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,14 @@ const arg = process.argv;
 if (arg.length < 4) {
   console.log(0);
 } else {
-    const arr = [];
-    let i = 2;
-    while (i < arg.length) {
-      arr[i - 2] = parseInt(arg[i]);
-      i++;
-    }
-    const max = Math.max.apply(null, arr);
-    arr.splice(arr.indexOf(max), 1);
-    const second = Math.max.apply(null, arr);
-    console.log(second);
+  const arr = [];
+  let i = 2;
+  while (i < arg.length) {
+    arr[i - 2] = parseInt(arg[i]);
+    i++;
   }
+  const max = Math.max.apply(null, arr);  
+  arr.splice(arr.indexOf(max), 1);
+  const second = Math.max.apply(null, arr);
+  console.log(second);
+}
