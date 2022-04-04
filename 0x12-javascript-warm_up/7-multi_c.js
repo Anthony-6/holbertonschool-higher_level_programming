cc80a38e179495dcd4d45e750f2e7d95a8444414
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-04 17:57:57 +0200

    fix text

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const arg = process.argv;
 const isInt = parseInt(arg[2]);
 if (isNaN(isInt)) {
-  console.log('Missing number of occurences');
+  console.log('Missing number of occurrences');
 } else { 
   for (i = 0; i < arg[2]; i++) {
     console.log('C is fun');
