4334372390f52ba4ee36a7746e7b1c770471a05a
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-04 17:45:15 +0200

    update file

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv;
-if (arg[2] === undefined) {
-    console.log('undefined is indefined');
-} else if (arg[3] === undefined) {
-    console.log(arg[2] + ' is undefined');
-} else {
-    console.log(arg[2] + ' is ' + arg[3]);
-}
+console.logs(arg[2] + ' is ' + arg[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const arg = process.argv;
 const isInt = parseInt(arg[2]);
 if (isNaN(isInt)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + isInt);
+  console.log('My number: ' + isInt);
 }
