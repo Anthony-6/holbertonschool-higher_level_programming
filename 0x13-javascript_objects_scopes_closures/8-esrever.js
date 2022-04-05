c1e971dd1000117ed58ce3ed7b9ea2a15fe1886f
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-05 18:19:59 +0200

    fix syntax

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const myList = [];
-  for (let i = list.length -1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     myList.push(list[i]);
   }
   return (myList);
