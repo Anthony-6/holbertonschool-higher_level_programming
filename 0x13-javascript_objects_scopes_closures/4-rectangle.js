1524e12748dfa2004b3ce26d8266c3af62457848
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-05 17:46:05 +0200

    add all file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+module.exports  = class Rectangle {
+  constructor(w, h) {
+    if (h > 0 && w > 0) {
+    this.width = w;
+    this.height = h;
+  }
+  }
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+  rotate () {
+    const saveheight = this.height;
+    this.height = this.width;
+    this.width = saveheight;
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,6 @@
+#!/user/bin/node
+let counter = 0;
+exports.logMe = function (item) {
+  console.log(counter + ': ' + item);
+  counter++;
+};
