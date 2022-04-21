3f810712830860c3a98dcd2ed8a916a95680ea8e
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-21 13:19:17 +0200

    update file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -iH "X-School-User-Id"="98" -sX POST $1
+curl -i -H -d "X-School-User-Id"="98" -sX GET $1
