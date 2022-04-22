e4ae329c05292b484a4b016cad225f068bf0e97a
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-22 13:03:40 +0200

    add file for task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     import requests
     import sys
-    
+
     req = requests.get(sys.argv[1])
     if req.status_code > 400:
         print("Error code: {}".format(req.status_code))
