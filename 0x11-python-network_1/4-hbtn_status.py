5304fbb16df3ee0fd5d6abf670e043eba555cf98
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-22 12:02:54 +0200

    add file for task 5

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''script that fetches an url using requests package'''
+if __name__ == "__main__":
+    import requests
+
+    request = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(request.text.__class__))
+    print('\t- content {}'.format(request.text))
