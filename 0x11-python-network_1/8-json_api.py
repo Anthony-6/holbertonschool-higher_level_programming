6d0ba2644577f05553932b0b22661323d2939535
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-22 14:12:39 +0200

    add file for task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter '''
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) == 1:
+        data = {'q': ''}
+    else:
+        data = {'q': argv[1]}
+    url = "http://5771634acedf.c90a0b50.hbtn-cod.io:5000/search_user"
+    req = requests.post(url, data)
+    try:
+        json = req.json()
+        if json == {}:
+            print("No result")
+        else:
+            print("[<{}>] <{}>".format(json.get('id'), json.get('name')))
+    except Exception:
+        print("Not a valid JSON")
