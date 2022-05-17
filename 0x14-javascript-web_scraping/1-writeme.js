21717f3c85716d1c4e7d38a286d2d13137b66cf5
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-17 12:32:15 +0200

    update

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+try {
+  fs.writeFile(process.argv[2], process.argv[3], function (err) {
+    if (err) console.log(err);
+  });
+} catch (err) {
+  console.log(err);
+}
