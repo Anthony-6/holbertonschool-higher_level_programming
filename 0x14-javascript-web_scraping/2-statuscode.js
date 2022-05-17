db344396dfcfd30334365b3a1c10e92bbda156be
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-17 12:33:33 +0200

    update file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@
 const axios = require('axios').default;
 
 axios.get(process.argv[1])
-    .catch(function (error) {
-        if (error.response) {
-            console.log('code :', error.response.status);
-        }
-    });
+  .catch(function (error) {
+    if (error.response) {
+      console.log('code :', error.response.status);
+    }
+  });
